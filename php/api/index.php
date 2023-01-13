e822c939d9b57feba832bea2ca0a42f962c13a6f
Sergey <sergeybulankin@gmail.com>
2023-01-13 23:31:52 +0500

    create development env

---
+++ php/api/index.php
@@ -0,0 +1,14 @@
+<?php
+    $db = [
+        ["name" => "Monika", "img" => "/services/monika.jpg"],
+        ["name" => "Eva",  "img" => "/services/eva.jpg"],
+        ["name" => "Amanda",  "img" => "/services/amanda.jpg"]
+    ];
+
+    header('Access-Control-Allow-Origin: *');
+    header("Content-type: application/json; charset=utf-8");
+    header("HTTP/1.1 200 Success");
+
+    echo json_encode($db);
+
+?>
