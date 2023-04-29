5d373f3fa2da4e1ea2982369b4ab7757ba69dda2
DaNiK2 <daniel.karamov@gmail.com>
2023-04-29 20:36:03 +0500

    Make working change favorite logo, read and write to data.

---
+++ nodejs/src/components/Favorite/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <favorite-button />
+</template>
+
+<script>
+import FavoriteButton from "@/components/Favorite/components/Favorite/FavoriteButton.vue";
+export default {
+  name: "App",
+  data() {
+    return {};
+  },
+
+  components: {
+    FavoriteButton,
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ nodejs/src/components/Favorite/components/Favorite/FavoriteButton.vue
@@ -0,0 +1,55 @@
+<template>
+  <li class="column is-4"></li>
+    <div v-for="(product, i) in products.productsList" :key="i">
+      <span v-if="product.favorite === true"
+      @click="deleteFromFavorite(product)"
+      >
+        <ion-icon name="heart-outline"></ion-icon>
+      </span>
+      <span v-else
+            @click="addToFavorite(product)"
+      >
+          <ion-icon name="heart-dislike-outline"></ion-icon>
+      </span>
+    </div>
+  <p v-for="(product, i) in products.productsList" :key="i">
+    {{ product }}
+  </p>
+</template>
+
+<script>
+//import { isProxy, toRaw } from 'vue';
+export default {
+  name: "FavoriteButton",
+  data: function () {
+    return {
+      products: {
+        productsList: [
+          {
+            product_id: 0,
+            favorite: false
+          },
+          {
+            product_id: 1,
+              favorite: true
+          },
+          {
+            product_id: 2,
+              favorite: false
+          },
+        ],
+      },
+
+    };
+  },
+  methods: {
+    addToFavorite: function (data) {
+        this.products.productsList[data.product_id].favorite = true;
+    },
+      deleteFromFavorite: function (data) {
+          this.products.productsList[data.product_id].favorite = false;
+      }
+  }
+};
+</script>
+<style scoped></style>
