41041b85f8240e80c1b0c3e8223fdf268466aa51
DaNiK2 <daniel.karamov@gmail.com>
2023-04-29 19:19:50 +0500

    Start working with favorite component

--- nodejs/src/App.vue
+++ nodejs/src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <header-component />
+  <favorite-button />
 </template>
 
 <script>
 import HeaderComponent from "./components/Header/App.vue";
+import FavoriteButton from "./components/Favorite/App.vue";
 
 export default {
   name: "App",
   components: {
     HeaderComponent,
+    FavoriteButton,
   },
 };
 </script>
