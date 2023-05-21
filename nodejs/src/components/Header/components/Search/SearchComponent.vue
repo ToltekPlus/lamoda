b85c1d053e5c512be4a9d02fe97aab47a7388b5c
DaNiK2 <daniel.karamov@gmail.com>
2023-05-21 19:03:36 +0500

    Start fix search

--- nodejs/src/components/Header/components/Search/SearchComponent.vue
+++ nodejs/src/components/Header/components/Search/SearchComponent.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="search column is-8">
     <p class="control has-icons-left">
-      <input
-        class="input is-hovered"
-        type="search"
-        placeholder="Найти товары"
-      />
-      <button @click="search(prodsList)">Найти</button>
-      <span class="icon is-small is-left">
+      <input class="input" type="search" v-model="searchText" placeholder="Найти товары" />
+      <span class="icon is-small is-right">
         <ion-icon name="search-outline" class="search-icon"></ion-icon>
       </span>
     </p>
+    <div class="resault" v-show="showResault">
+        <p v-if="this.searchResault.length > 0">
+            {{ searchResault }}
+        </p>
+        <p v-else>
+            Товары не найдены.
+        </p>
+    </div>
   </div>
 </template>
 
@@ -19,13 +22,28 @@ export default {
   name: "SearchComponent",
   props: ["prodsList"],
   data() {
-    return {};
+    return {
+      showResault: false,
+        searchText:'',
+        searchResault: ''
+
+    }
   },
+    watch: {
+        searchText() {
+          if(this.searchText.trim().length >= 3){
+              this.showResault = true;
+              this.search();
+          } else {
+              this.showResault = false;
+          }
+      }
+    },
   methods: {
-    search(list) {
+    search() {
       let searchItem = document.querySelector("input"); // Забираем input
       const sortedList = [];
-      for (const item of list.testSearch) {
+      for (const item of this.prodsList.testSearch) {
         // Перебираем объект
         if (
           item.product_name
@@ -36,8 +54,25 @@ export default {
           sortedList.push(item.id); // Записываем в массив ключи
         }
       }
-      console.log(sortedList);
+      this.searchResault = sortedList;
     },
   },
 };
 </script>
+
+<style>
+.control .input {
+  padding-left: 11.75px !important;
+  padding-right: 2.5em;
+}
+.icon {
+  right: 0;
+}
+.resault {
+  position: fixed;
+  height: 300px;
+  background-color: #F1F1F1;
+  box-shadow: inset 0 0.0625em 0.125em rgba(10,10,10,0.05);
+  width: auto;
+}
+</style>
